07891a70cad2e0cd06ef00728d17067f2e6a9b9d
1437971901 <45599498+1437971901@users.noreply.github.com>
2018-12-09 21:02:22 +0800

    Add files via upload

---
+++ jquery.cookie.js
@@ -0,0 +1,75 @@
+﻿jQuery.cookie = function (name, value, options) {
+    if (typeof value != 'undefined') { // name and value given, set cookie
+        options = options || {};
+        if (value === null) {
+            value = '';
+            options.expires = -1;
+        }
+        var expires = '';
+        if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
+            var date;
+            if (typeof options.expires == 'number') {
+                date = new Date();
+                date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
+            } else {
+                date = options.expires;
+            }
+            expires = '; expires=' + date.toUTCString(); // use expires attribute, max-age is not supported by IE
+        }
+        var path = options.path ? '; path=' + options.path : '';
+        var domain = options.domain ? '; domain=' + options.domain : '';
+        var secure = options.secure ? '; secure' : '';
+        document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
+    } else { // only name given, get cookie
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+};
+jQuery.getDNZCookie = function (name) {
+    var v = null;
+    var cookie_start = document.cookie.indexOf(name);
+    if (cookie_start > 0) {
+        var s = document.cookie.substring(cookie_start);
+        var cookie_end = document.cookie.indexOf("&", cookie_start);
+        return cookie_start == -1 ? '' : unescape(document.cookie.substring(cookie_start + name.length + 1, (cookie_end > cookie_start ? cookie_end : document.cookie.length)));
+    }
+    return v;
+};
+function getcookie(name) {
+    var cookie_start = document.cookie.indexOf(name);
+    var cookie_end = document.cookie.indexOf(";", cookie_start);
+    return cookie_start == -1 ? '' : unescape(document.cookie.substring(cookie_start + name.length + 1, (cookie_end > cookie_start ? cookie_end : document.cookie.length)));
+}
+function setcookie(cookieName, cookieValue, seconds, path, domain, secure) {
+    var expires = new Date();
+    expires.setTime(expires.getTime() + seconds);
+    document.cookie = escape(cookieName) + '=' + escape(cookieValue)
++ (expires ? '; expires=' + expires.toGMTString() : '')
++ (path ? '; path=' + path : '/')
++ (domain ? '; domain=' + domain : '')
++ (secure ? '; secure' : '');
+}
+var searchkey = "搜索输入产品名、货号或者CAS号";
+function onblurKey(o) {
+    if (o.value == '') { o.value = searchkey }
+}
+function onfocuskey(o) {
+    if (o.value == searchkey) { o.value = '' }
+    return true;
+}
+function onchangeKey(o) {
+    if (o.value != "" && o.value != searchkey) {
+
+    }
+}
